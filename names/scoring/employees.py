f021b6d6a28af72b4391947ece0ba9b9319a5c0d
Mike Moss <mrmoss@alaska.edu>
2016-04-15 17:06:57 -0800

    Scoring engine up.

---
+++ names/scoring/employees.py
@@ -0,0 +1,150 @@
+#!/usr/bin/env python
+import hashlib
+import json
+
+departments=[
+	('MG',49,'Management'),
+	('HR',55,'Human Resources'),
+	('AC',33,'Accounting'),
+	('JA',40,'Janitorial'),
+	('SC',51,'Security'),
+	('IT',58,'Information Technology')]
+
+class department_t:
+	def __init__(self,data=None):
+		global departments
+		self.initials='NA'
+		self.number=0
+		self.name="Invalid"
+		if type(data)==str:
+			if len(data)==2:
+				for department in departments:
+					if department[0]==data:
+						self.initials=department[0]
+						self.number=department[1]
+						self.name=department[2]
+			else:
+				for department in departments:
+					if department[2]==data:
+						self.initials=department[0]
+						self.number=department[1]
+						self.name=department[2]
+		if type(data)==int:
+			for department in departments:
+				if department[1]==data:
+					self.initials=department[0]
+					self.number=department[1]
+					self.name=department[2]
+
+class employee_t:
+	def __init__(self,first=None,last=None):
+		if first!=None and last!=None:
+			self.first=first
+			self.last=last
+			self.username=(self.first[0]+self.last).lower()
+
+	def json(self,authorized=False,ordered=False):
+		obj={}
+		prefix=''
+		if ordered:
+			prefix=0
+		obj[str(prefix)+'Full Name']=self.first+" "+self.last
+		if ordered:
+			prefix+=1
+		obj[str(prefix)+'First Name']=self.first
+		if ordered:
+			prefix+=1
+		obj[str(prefix)+'Last Name']=self.last
+		if ordered:
+			prefix+=1
+		obj[str(prefix)+'Email']=self.username+'@lawl.org'
+		if ordered:
+			prefix+=1
+		obj[str(prefix)+'Department']=self.department.name
+		if ordered:
+			prefix+=1
+		if authorized:
+			obj[str(prefix)+'ID']=self.id
+			if ordered:
+				prefix+=1
+			obj[str(prefix)+'Username']=self.username
+			if ordered:
+				prefix+=1
+			obj[str(prefix)+'Department Number']=self.department.number
+			if ordered:
+				prefix+=1
+			obj[str(prefix)+'Department Initials']=self.department.initials
+			if ordered:
+				prefix+=1
+			obj[str(prefix)+'Issue Number']=self.issue
+		return obj
+
+	def csv(self,authorized=False):
+		csv=''
+		delim=','
+		csv+=str(self.id)+delim
+		csv+=str(self.department.number)+delim
+		csv+=self.department.initials+delim
+		csv+=str(self.issue)+delim
+		csv+=self.first+delim
+		csv+=self.last+delim
+		csv+=self.username+delim
+		csv+=self.department.name
+		return csv
+
+	def arr(self,authorized=False):
+		arr=[]
+		arr.append(self.first.lower())
+		arr.append(self.last.lower())
+		arr.append(str(self.id).lower())
+		arr.append(self.username.lower())
+		return arr
+
+def search(database,terms,authorized=False,ordered=False):
+	results=[]
+	for term in terms:
+		if len(term)>3:
+			for employee in database:
+				values=employee.arr()
+				for ii in range(len(values)):
+					if (ii<2 and term in values[ii]) or term==values[ii]:
+						results.append(employee.json(authorized,ordered))
+						break
+	return str(json.dumps(results))
+
+def gen_from_names(filename):
+	database=[]
+	delim=','
+	file=open(filename,'r')
+	for line in file:
+		line=line.strip()
+		line=line.split();
+		if len(line)==2:
+			database.append(employee_t(line[0],line[1],True))
+	return database
+
+def load_from_csv(filename):
+	database=[]
+	file=open(filename,'r')
+	for line in file:
+		line=line.strip()
+		line=line.split(',')
+		if len(line)==8:
+			employee=employee_t()
+			employee.id=line[0]
+			employee.department=department_t()
+			employee.department.number=zero_padded_int(line[1])
+			employee.department.initials=line[2]
+			employee.issue=zero_padded_int(line[3])
+			employee.first=line[4]
+			employee.last=line[5]
+			employee.username=line[6]
+			employee.department.name=line[7]
+			database.append(employee)
+	return database
+
+def zero_padded_int(string):
+	ii=str(string).strip().lstrip('0')
+	if len(ii)==0:
+		ii='0'
+	return int(ii)
---
+++ names/scoring/scoring.py
@@ -0,0 +1,78 @@
+#!/usr/bin/env python
+import SimpleHTTPServer
+import BaseHTTPServer
+import employees
+import json
+import re
+import sys
+import urllib
+import urlparse
+
+database=[]
+flag_db={}
+
+class MyHandler(SimpleHTTPServer.SimpleHTTPRequestHandler):
+	def do_GET(self):
+		try:
+			global database
+			self.path='web/'+self.path
+			query_str=urlparse.urlparse(self.path).query
+
+			if len(query_str)==0:
+				return SimpleHTTPServer.SimpleHTTPRequestHandler.do_GET(self)
+			else:
+				for query in query_str.split('&'):
+					query=query.split('=')
+					if len(query)>1:
+						key=query[0]
+						val=urllib.unquote(query[1])
+						if key=='id':
+							return SimpleHTTPServer.SimpleHTTPRequestHandler.do_GET(self)
+						if key=='search':
+							val=re.sub('[^0-9a-zA-Z]+',' ',val).lower().split(' ')
+							self.send_response(200)
+							self.send_header('Content-type','text/html')
+							self.end_headers()
+							obj=employees.search(database,val,False,True)
+							if len(obj)>2:
+								obj_parsed=json.loads(obj)
+								if len(obj_parsed)>0:
+									if not val[0] in flag_db:
+										flag_db[val[0]]='00000000'
+									obj_parsed[0]['flags']=flag_db[val[0]]
+									obj=json.dumps(obj_parsed)
+							self.wfile.write(str(obj))
+							self.wfile.close()
+						if key=='flag':
+							obj='{}'
+							id=val[:6]
+							num=val[6:]
+							if len(num)>0:
+								num=int(num)
+							if len(id)==6 and num>=0 and num<=7:
+								obj=employees.search(database,[id],False,True)
+								if len(obj)>2:
+									if not id in flag_db:
+										flag_db[id]='00000000'
+									flag_db[id]=flag_db[id][:num]+'1'+flag_db[id][num+1:]
+							self.send_response(200)
+							self.send_header('Content-type','text/html')
+							self.end_headers()
+							self.wfile.write('')
+							self.wfile.close()
+		except Exception as error:
+			self.send_response(400)
+			self.end_headers()
+			self.wfile.close()
+			sys.stderr.write(str(error)+'\n')
+			pass
+
+if __name__=="__main__":
+	try:
+		database=employees.load_from_csv('names.csv')
+		Handler=MyHandler
+		server=BaseHTTPServer.HTTPServer(('0.0.0.0',8081),MyHandler)
+		server.serve_forever()
+	except Exception as error:
+		sys.stderr.write(str(error)+'\n')
+		exit(1)
