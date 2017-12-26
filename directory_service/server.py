f51b97b7f3565807bba0ab6b028b6806c1db3d46
Mike Moss <doomsday.wombats@gmail.com>
2017-12-26 13:21:50 -0900

    Made server slightly more secure.

--- directory_service/server.py
+++ directory_service/server.py
@@ -1,7 +1,9 @@
-#!/usr/bin/env python
+#!/usr/bin/env python2
 import SimpleHTTPServer
 import BaseHTTPServer
 import employees
+import os
+import mimetypes
 import re
 import sys
 import urllib
@@ -13,12 +15,10 @@ class MyHandler(SimpleHTTPServer.SimpleHTTPRequestHandler):
 	def do_GET(self):
 		try:
 			global database
-			self.path='web/'+self.path
-			query_str=urlparse.urlparse(self.path).query
-
-			if len(query_str)==0:
-				return SimpleHTTPServer.SimpleHTTPRequestHandler.do_GET(self)
-			else:
+			self.path=self.path.split('?')
+			query_str='?'.join(self.path[1:])
+			if len(query_str)>0:
+				print(query_str)
 				for query in query_str.split('&'):
 					query=query.split('=')
 					if len(query)>1:
@@ -31,12 +31,31 @@ class MyHandler(SimpleHTTPServer.SimpleHTTPRequestHandler):
 							self.end_headers()
 							self.wfile.write(str(employees.search(database,val,False,True)))
 							self.wfile.close()
-		except Exception as error:
-			self.send_response(400)
+							return
+			self.path=self.path[0]
+			if len(self.path)>0 and self.path[-1]=='/':
+				self.path+='index.html'
+			cwd=os.getcwd()+'/web/'
+			self.path=os.path.abspath(cwd+self.path)
+			if self.path.find(cwd)!=0 or not os.path.isfile(self.path):
+				self.send_response(404)
+				self.end_headers()
+				return
+			file=open(self.path,'r')
+			self.send_response(200)
+			mime=mimetypes.guess_type(self.path)
+			if len(mime)>0:
+				mime=mime[0]
+			else:
+				mime='text/plain'
+			print(mime+' '+self.path)
+			self.send_header('Content-type',mime)
 			self.end_headers()
+			self.wfile.write(file.read())
 			self.wfile.close()
-			sys.stderr.write(str(error)+'\n')
-			pass
+		except Exception as error:
+			print(error)
+			self.send_response(401)
 
 if __name__=="__main__":
 	try:
